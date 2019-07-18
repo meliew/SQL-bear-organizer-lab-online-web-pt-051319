e8630f475c105f7cfacf0c32fd128bee9fcc8014
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-18 19:23:15 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'gray', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, 'male', 'blue', 'wild', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'female', 'purple', 'joyful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'male', 'brown', 'serious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'rainbow', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'male', 'green', 'mean', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'female', 'silver', 'silly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 3, 'male', 'polka-dot', 'vengeful', 0);
