de269579400978f779ed7ae410cfe95043e1d61c
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-18 18:42:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'gray', 'angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, 'male', 'blue', 'wild', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'female', 'purple', 'joyful', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'male', 'brown', 'serious', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'rainbow', 'happy', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'male', 'green', 'mean', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'female', 'silver', 'silly', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 3, 'male', 'polka-dot', 'vengeful', false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'male', 'gray', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 7, 'male', 'blue', 'wild', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 4, 'female', 'purple', 'joyful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 5, 'male', 'brown', 'serious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'female', 'rainbow', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'male', 'green', 'mean', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'female', 'silver', 'silly', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 3, 'male', 'polka-dot', 'vengeful', 0);
